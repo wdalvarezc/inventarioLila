ddf2a427321182da43e066406a42cd751918307d
Billycode <wdalvarezchia@gmail.com>
2025-09-08 22:14:58 -0500

    deploy a heroku

--- src/services/whatsappClient.js
+++ src/services/whatsappClient.js
@@ -7,7 +7,13 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"], // 👈 importante
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-gpu"], // 👈 importante
   },
 });
 
