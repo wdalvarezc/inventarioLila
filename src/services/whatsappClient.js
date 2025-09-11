65def91bed97cbfef8684de2c4e7ea7975454f21
Billycode <wdalvarezchia@gmail.com>
2025-09-11 11:27:32 -0500

    ajustes nuevos 2

--- src/services/whatsappClient.js
+++ src/services/whatsappClient.js
@@ -14,7 +14,6 @@ export const initWhatsApp = async () => {
     const sessionData = sessionRow ? sessionRow.session : null;
 
     const client = new Client({
-      authStrategy: new LocalAuth(),
       puppeteer: {
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
